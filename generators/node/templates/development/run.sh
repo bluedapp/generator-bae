aa1fcd4ed512edb92cb0dc225fd73698925466c9
demohi <mds@xue.bi>
2016-06-23 14:06:09 +0800

    add localidc

--- generators/node/templates/development/run.sh
+++ generators/node/templates/development/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+service qconf-agent restart
 pm2  --no-daemon start /usr/src/app/development/pm2.yml
--- generators/node/templates/production/run.sh
+++ generators/node/templates/production/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+service qconf-agent restart
 pm2  --no-daemon start /usr/src/app/production/pm2.yml
